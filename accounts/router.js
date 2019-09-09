ee4a953ef95962c3a5f131f30b8b8ee63d4111fc
Noor Al Darraji <077068@yahoo.com>
2019-09-09 17:46:25 -0500

    Completed the method "GET" using id  :hammer:

--- accounts/router.js
+++ accounts/router.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const db = require('../data/dbConfig')
 
 router.get('/', (req, res) => {
-    db('accounts')
+    db('accounts') // === select * from ('$1')
         .then(results => {
             res
                 .json(results)
@@ -14,6 +14,24 @@ router.get('/', (req, res) => {
                 .json({ errorMessage: "Couldn't get the accounts" })
         }
         )
+})
+
+router.get('/:id', (req, res) => {
+    const userId = req.params.id
+    console.log(userId)
+    db('accounts')
+        .where({ id: userId })
+        .first()
+        .then(account => {
+            if (account) {
+                res
+                    .json(account)
+            } else {
+                res
+                    .status(404)
+                    .json({ message: 'Unable to find the account' })
+            }
+        })
 })
 
 module.exports = router
